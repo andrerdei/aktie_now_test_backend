070c181b901334a06b3da21c04ba1acc749d22ac
Andre Erdei <andre.erdei@tevec.com.br>
2021-01-24 22:58:15 -0300

    ...

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const connection = process.env.DB_HOST ? process.env.DB_HOST : 'mongodb://localhost/nodedb';
 
-mongoose.connect(process.env.DB_HOST ? process.env.DB_HOST : 'mongodb://localhost/nodedb', {
+console.log(process.env.DB_HOST)
+
+mongoose.connect(connection, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
